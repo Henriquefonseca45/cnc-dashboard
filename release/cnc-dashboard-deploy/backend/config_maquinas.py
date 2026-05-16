1fbb3d53ed0b6987ffcffde1775bfa3cca9826a5
Henrique Fonseca <henriquesantos5614@gmail.com>
2026-05-16 07:47:01 -0300

    cleanup projeto

--- release/cnc-dashboard-deploy/backend/config_maquinas.py
+++
@@ -1,6 +0,0 @@
-MAQUINAS = {
-    "CNC01": r"C:\PLANOS-CNC01",
-    # depois você adiciona:
-    # "CNC02": r"C:\PLANOS-CNC02",
-    # ...
-}
--- release/cnc-dashboard-deploy/frontend/src/http.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const API_URL = import.meta.env.VITE_API_URL || "";
-
-export const http = axios.create({
-  baseURL: API_URL,
-});
